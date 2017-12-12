5fb23fe52a1cbc76fbb1a5877b852f4dbef115e1
macamarambio <marambio.macarena@gmail.com>
2017-12-12 09:02:42 -0300

    ejercicio agenda contactos

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,12 +11,12 @@ $(document).ready(function () {
 /*Agregar contacto*/
 		$('#contactos').append('<div class="col s12 m12 l12 contact_' + idContacto +'">'+
 			'<div class="container">'+
-			'<div class="card horizontal margenAlto">'+
+			'<div class="card horizontal">'+
 			'<div class="card-stacked">'+
 			'<div class="card-content">'+
 			'<h4>' + name + '</h4>' +
 			'<p>' + telefono + '</p>' +
-			'<button type="submit" class="waves-effect waves-light btn right btn-top"><i class="material-icons" id= "trash">delete</i></button>' +
+			'<button type="submit" class="waves-effect waves-light btn-top btn right"><i class="material-icons" id= "trash">delete</i></button>' +
 			'</div>'+
 			'</div>'+
 			'</div>'+
